126a91b920fc3627bb1e7c88eeb1c4931b96509a
SidArthurWilliams <sidwilliams909@gmail.com>
2024-09-25 19:30:32 +1200

    Tailwind fix

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,18 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        // Your custom colors
+        'eerie-black': {
+          DEFAULT: '#1B1B1B',
+          '900': '#0D0D0D',
+        },
+        'coquelicot': {
+          '400': '#FF6F59',
+          '500': '#FF5733',
+        },
+        'beige': {
+          DEFAULT: '#F5F5DC',
+          '500': '#F0E68C',
+        },
       },
     },
   },
